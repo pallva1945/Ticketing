ea1bb43ecad32e3bba793770aad1d43a1bf73e93
LS4 <luisscola@pallacanestrovarese.it>
2026-01-04 15:55:53 +0100

    feat: Adjust widget layout for better vertical distribution
    Enhance the vertical widget column by adjusting flex properties. The `DistressedZones` widget now has a higher flex grow value (`flex-[2]`), giving it more space compared to the `CompKillerWidget` (`flex-1`). This improves the visual hierarchy and distribution of content in the sidebar.

--- App.tsx
+++ App.tsx
@@ -1624,8 +1624,8 @@ const App: React.FC = () => {
                                 </div>
 
                                 {/* Vertical Widgets Column */}
-                                <div className="xl:col-span-1 flex flex-col gap-6 h-[600px]">
-                                    <div className="flex-1 min-h-0">
+                                <div className="xl:col-span-1 flex flex-col gap-4 h-[600px]">
+                                    <div className="flex-[2] min-h-0">
                                         <DistressedZones data={viewData} />
                                     </div>
                                     <div className="flex-1 min-h-0">
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+                                {/* Vertical Widgets Column */}
+                                <div className="xl:col-span-1 flex flex-col gap-4 h-[600px]">
+                                    <div className="flex-[2] min-h-0">
+                                        <DistressedZones data={viewData} />
+                                    </div>
+                                    <div className="flex-1 min-h-0">
+                                        <CompKillerWidget data={viewData} />
+                                    </div>
+                                </div>
